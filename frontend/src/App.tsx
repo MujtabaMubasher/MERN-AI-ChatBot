84f638082a71c66dc17769303bbc74d197f93898
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-05-15 18:12:38 +0000

    Done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import { Home, Chat, Login, Signup, NotFound } from "./pages";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element = {<Home/>} />
+        <Route path="/login" element = {<Login/>} />
+        <Route path="/signup" element = {<Signup/>} />
+        <Route path="/chat" element = {<Chat/>} />
+        <Route path="*" element = {<NotFound/>} />
+      </Routes>
+    </main>
+  );
+}
+
+export default App;
